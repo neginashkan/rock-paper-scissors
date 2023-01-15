3c954b44e8c40f1d6f0ebaa076f718bfe4003a53
negnq <117580177+neginashkan@users.noreply.github.com>
2023-01-15 12:22:59 +0330

    Update CSS,HTML,javaScript
    add some javaScript
    and fix problems in HTML and  CSS for being responsible

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,18 @@
 const buttons = document.querySelectorAll(".game-btn");
+const startGameBtn = document.getElementById("start-game-btn");
+const userChoiceHtml = document.getElementById("user-choice-html");
+const resultHtml = document.getElementById("result-html");
+const tryAginBtn = document.getElementById("try-agin-btn");
+tryAginBtn.addEventListener("click", function () {
+  userChoiceHtml.style.display = "block";
+  resultHtml.style.display = "none";
+});
+function visibilityOn() {
+  const startGameHtml = document.getElementById("start-game-html");
+  userChoiceHtml.style.display = "block";
+  startGameHtml.style.display = "none";
+}
+startGameBtn.addEventListener("click", visibilityOn);
 function show(event) {
   game(event.target.id);
 }
@@ -9,11 +23,17 @@ function computerChoice() {
   return choices[randomIndex];
 }
 function game(user) {
+  userChoiceHtml.style.display = "none";
+  resultHtml.style.display = "block";
   const userImage = document.getElementById("user-img");
   const computerImage = document.getElementById("computer-img");
   const userEl = document.getElementById("user");
   const computerEl = document.getElementById("computer");
   let computer = computerChoice();
+  userImage.src = `/images/${user}.png`;
+  computerImage.src = `/images/${computer}.png`;
+  userImage.classList.add(`.${user}-user`);
+  computerImage.classList.add(`.${computer}-computer`);
   userEl.textContent = user;
   computerEl.textContent = computer;
   const resultEl = document.getElementById("result");
